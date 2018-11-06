edb9cb9b190c72c93a8f57d6f6e987a450aaf0a5
FrameSpark <432032@mail.ru>
2018-11-06 22:24:30 +0300

    add func to LentMatrix

--- src/main/java/LentMatrix.java
+++ src/main/java/LentMatrix.java
@@ -1,13 +1,15 @@
+import java.util.Random;
+
 public class LentMatrix {
 
-    double[][] A;
-    double [][]B;
-    double [][]C;
-    double []x;
-    double []y;
-    double []f;
-    double []exactX;
-    int N,L;
+    double[][] A; //Лента N*(2L-1)
+    double [][]BС; //нижне и верхнетреугольные матрицы N*(2L-1)
+    double []x; //вектор решения
+    double []y; //вектор решения
+    double []f; //правая часть
+    double []exactX; //точное решение
+    int N,L; //размерность системы и половина ширины ленты
+    Random rand = new Random();
     LentMatrix(int N, int L) {
         this.N = N;
         this.L = L;
@@ -15,7 +17,162 @@ public class LentMatrix {
         y = new double[N+1];
         exactX = new double[N+1];
         A=new double[N+1][2*L];
-        B= new double[N+1][2*L];
-        C = new double[N+1][2*L];
+        BС= new double[N+1][2*L];
     }
+
+
+ //верхне и нижнетреугольная матрица A, используется для fillBadMatrix
+    double getLow(int i,int j){
+        if (Math.abs(j - i+L) >= 2*L) //если вышли за границы ленты
+            return 0;
+        if (Math.abs(i - j+L) >= 2*L) //если вышли за границы ленты
+            return 0;
+
+        if (j - i > 0) //если обращение к верхней части
+            return 0;
+        else //если нижняя часть
+            return A[j][i - j + L];
+    }
+
+    double getHigh(int i,int j) {
+        if (Math.abs(j - i + L) >= 2 * L) //если вышли за границы ленты
+            return 0;
+        if (Math.abs(i - j + L) >= 2 * L) //если вышли за границы ленты
+            return 0;
+        if (j - i >= 0) //если обращение к верхней части
+            return A[i][j - i + L];
+        else //если нижняя часть
+            return 0;
+    }
+    
+
+    //Побочные функции
+    double randomDouble(int left,int right){
+
+        double z=0;
+        double z1=0;
+
+        //while(z==0.0){
+       //         z=rand.nextDouble();
+         //   }
+            while(z1==0){
+                z1 = rand.nextInt(right)+left;
+            }
+        return z+z1;
+    }
+   //Умножение матрицы A на заданный вектор точного решения
+    void mulByExact()
+    {
+        for (int i = 1; i <= N; i++)
+        {
+            f[i] = 0;
+            for (int j = 1; j <= N; j++)
+                f[i] += exactX[j] * getA(i, j);
+        }
+    }
+
+
+    //Обращения к матрицам
+    double getA(int i, int j){
+        if (Math.abs(j - i+L) >= 2*L) //если вышли за границы ленты
+            return 0;
+        if (Math.abs(i - j+L) >= 2*L) //если вышли за границы ленты
+            return 0;
+        if (j - i >= 0) //если обращение к верхней части
+            return A[i][j - i + L];
+        else //если нижняя часть
+            return A[j][i - j + L];
 }
+
+ void setA(double value,int i, int j){
+     if (Math.abs(j - i+L) >= 2*L) //если вышли за границы ленты
+         return;
+     if (Math.abs(i - j+L) >= 2*L) //если вышли за границы ленты
+         return ;
+     if (j - i >= 0) //если обращение к верхней части
+         A[i][j - i + L] = value;
+     else //если нижняя часть
+         A[j][i - j + L] = value;
+ }
+
+ double getBC(int i,int j){
+        if(Math.abs(j-i+L) >= 2*L)
+            return 0;
+        return BС[i][j-i+L];
+ }
+
+ void setBC(double value, int i, int j){
+        if(Math.abs(j-i+L) >= 2*L){
+            return;
+        }
+        BС[i][j-i+L]=value;
+ }
+
+    void fillA(int left,int right){
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=(2*L-1);j++){
+                A[i][j]=randomDouble(left,right);
+            }
+        }
+    }
+
+    void fillExactX(int left,int right){
+        for(int i=1; i<=N;i++){
+            exactX[i]=randomDouble(left,right);
+        }
+    }
+
+    //Вывод на экран
+    void showA(){
+        System.out.println("A matrix: ");
+        for(int i=1;i<=N;i++){
+            for(int j=1; j<=N; j++){
+                System.out.print(getA(i,j) + " ");
+            }
+            System.out.println();
+        }
+    }
+    void showBC(){
+        System.out.println("BC matrix: ");
+        for (int i=1; i<=N;i++){
+            for(int j=1; j<=N;j++){
+                System.out.print(getBC(i,j) + " ");
+            }
+        }
+    }
+
+    void showExactX()
+    {
+        System.out.println("Exact X: ");
+        for (int i = 1; i <= N; i++)
+            System.out.println(exactX[i]);
+
+    }
+    void showX()
+    {
+        System.out.println("Solution: " );
+        for (int i = 1; i <= N; i++)
+            System.out.println(x[i]);
+
+    }
+
+    void resetData(){ //сброс данных
+        f = null;
+        x = null;
+        y = null;
+        exactX = null;
+        A=null;
+        BС =  null;
+        x = new double[N+1];
+        y = new double[N+1];
+        exactX = new double[N+1];
+        A=new double[N+1][2*L];
+        BС= new double[N+1][2*L];
+
+    }
+
+
+
+
+}
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        LentMatrix lent = new LentMatrix(6,3);
+        lent.fillA(1,4);
+        //lent.showA();
+        lent.showBC();
 
     }
 }
